ccda0ec8b8bfafb784f69c35a7de45722ef0fa6e
pawash <cs21.pawash.sharaf@lcit.edu.in>
2023-10-25 18:06:57 +0530

    api

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
