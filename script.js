800895edccefbbeb82193b2c976e5d3e69925c22
Pawash Sharaf <99664480+pawashsharaf@users.noreply.github.com>
2023-12-07 21:48:34 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,44 +5,87 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-    chatLi.innerHTML = chatContent;
-    chatLi.querySelector("p").textContent = message;
-    return chatLi; // return chat <li> element
-}
+  // Create a chat <li> element with the passed message and className
+  const chatLi = document.createElement("li");
+  chatLi.classList.add("chat", `${className}`);
+  let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">settings_accessibility</span><p></p>`;
+  chatLi.innerHTML = chatContent;
+  chatLi.querySelector("p").textContent = message;
+  return chatLi; // return chat <li> element
+};
 
 const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
-    const messageElement = chatElement.querySelector("p");
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+  const messageElement = chatElement.querySelector("p");
 
-    // Define the properties and message for the API request
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-        })
-    }
+  // Define the properties and message for the API request
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: userMessage }],
+    }),
+  };
 
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
+  // Check for specific queries
+  if (userMessage.toLowerCase().includes("who created you")) {
+    messageElement.textContent = "I was developed by pawash & Designed by OpenAI";
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    return;
+  }
+  if (userMessage.toLowerCase().includes("who created u")) {
+    messageElement.textContent = "I was developed by pawash & Designed by OpenAI";
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    return;
+  }
+  if (userMessage.toLowerCase().includes("who create you")) {
+    messageElement.textContent = "I was developed by pawash & Designed by OpenAI";
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    return;
+  }
+  if (userMessage.toLowerCase().includes("who create u")) {
+    messageElement.textContent = "I was developed by pawash & Designed by OpenAI";
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    return;
+  }
+  if (userMessage.toLowerCase().includes("who developed you")) {
+    messageElement.textContent = "I was developed by pawash & Designed by OpenAI";
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    return;
+  }
+  if (userMessage.toLowerCase().includes("who developed u")) {
+    messageElement.textContent = "I was developed by pawash & Designed by OpenAI";
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    return;
+  }
+  if (userMessage.toLowerCase().includes("who devlope u")) {
+    messageElement.textContent = "I was developed by pawash & Designed by OpenAI";
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    return;
+  }
+
+
+  // Send POST request to API, get response, and set the response as paragraph text
+  fetch(API_URL, requestOptions)
+    .then((res) => res.json())
+    .then((data) => {
+      messageElement.textContent = data.choices[0].message.content.trim();
+    })
+    .catch(() => {
+      messageElement.classList.add("error");
+      messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    })
+    .finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+};
+
 
 const handleChat = () => {
     userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
@@ -58,12 +101,12 @@ const handleChat = () => {
     
     setTimeout(() => {
         // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        const incomingChatLi = createChatLi("Fetching result.....", "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
         generateResponse(incomingChatLi);
     }, 600);
-}
+};
 
 chatInput.addEventListener("input", () => {
     // Adjust the height of the input textarea based on its content
